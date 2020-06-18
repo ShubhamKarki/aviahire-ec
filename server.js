ef6bc9b3bc0ac79587bda83ba30d8ab0f55cee02
Shubham karki <shubhamkarkiworkings@gmail.com>
2020-06-18 12:39:08 +0530

    added cros enable

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ const handleTwitter = require("./handleTwitter");
 // Import cheerio
 const githubRepoUsers = require("./github_repos_user");
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
 
 app.post("/search", async (req, res) => {
